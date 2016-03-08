59b89cc8b37d95b169dcce222677797a18206bb9
Jiaming Suen <suenjiaming@gmail.com>
2016-03-09 01:25:40 +0800

    Network Speed Monitor

--- ubuntu_config.sh
+++ ubuntu_config.sh
@@ -2,15 +2,15 @@
 
 #This is a config script for Ubuntu, customized especially for myself.
 #Before running the script, determine the platform is x64 or arm, and the version of ROS.
-
+#Before running, sudo -s and apt-get update
 
 mkdir ~/Repositories
 cd ~/Repositories
 
 #oh-my-zsh
-sudo -s
-apt-get update
-apt-get install vim zsh git-core build-essential autoconf libtool libssl-dev polipo
+#sudo -s
+#apt-get update
+apt-get -y install vim zsh git-core build-essential autoconf libtool libssl-dev polipo
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 chsh -s `which zsh`
 
@@ -30,20 +30,24 @@ sudo cat >/etc/shadowsocks.json <<EOL
     "method":"aes-128-cfb"
 }
 EOL
-nohup ss-local -c /etc/shadowsocks.json &&
+ss-local -c /etc/shadowsocks.json &
 
 
 #polipo
 service polipo stop
-nohup polipo socksParentProxy=localhost:1080 &&
+polipo socksParentProxy=localhost:1080 &
 export http_proxy=localhost:1080
 
 
 #Chrome
 #TO-DO: Detect arm/x64 and figure out Chrome or Chromium
+#x64
 apt-get install libxss1 libappindicator1 libindicator7
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome*.deb
+#arm
+#sudo apt-get install chromium-browser
+
 
 #ROS
 
@@ -52,7 +56,13 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 0xB01FA116
 sudo apt-get install libgl1-mesa-dev-lts-utopic
 #Modify ROS version here.
+sudo apt-get update
 sudo apt-get install ros-jade-desktop-full
+
+#Network Speed Monitor
+sudo add-apt-repository ppa:nilarimogard/webupd8
+sudo apt-get update
+sudo apt-get install indicator-netspeed
 
 #arm (Could be a little slippery here.)
 #sudo update-locale LANG=C LANGUAGE=C LC_ALL=C LC_MESSAGES=POSIX
